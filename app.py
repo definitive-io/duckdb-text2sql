77427156d90584c8b50fb31f2432c65361248b3f
dloman118 <99347459+dloman118@users.noreply.github.com>
2024-02-16 18:04:45 -0500

    update app and requirements

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from groqcloud import Groqcloud
+from groq import Groq
 import json
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -9,29 +9,20 @@ import duckdb
 import sqlparse
 
 
-def get_conversational_history(base_prompt,user_question_history,chatbot_answer_history,memory_length):
-    
-    user_question_history = user_question_history[memory_length*-1:]
-    chatbot_answer_history = chatbot_answer_history[memory_length*-1:]
-    if len(chatbot_answer_history) > 0:
-        conversational_history = '''
-        
-        As a recap, here is the current conversation:
-            
-        ''' + "\n".join(f"Human: {q}\nAI: {a}" for q, a in zip(user_question_history, chatbot_answer_history))
-
-        full_prompt = base_prompt + conversational_history + '''
-            Human: {user_question}
-            AI:
-        '''.format(user_question = user_question_history[-1])
-    else:
-        full_prompt = base_prompt.format(user_question = user_question_history[-1])
-    
-    return full_prompt
-
 
 def chat_with_groq(client,prompt,model):
-    
+    """
+    This function sends a prompt to the Groq API and retrieves the AI's response.
+
+    Parameters:
+    client (Groq): The Groq API client.
+    prompt (str): The prompt to send to the AI.
+    model (str): The AI model to use for the response.
+
+    Returns:
+    str: The content of the AI's response.
+    """
+
     completion = client.chat.completions.create(
     model=model,
     messages=[
@@ -46,7 +37,15 @@ def chat_with_groq(client,prompt,model):
 
 
 def execute_duckdb_query(query):
+    """
+    This function executes a SQL query on a DuckDB database and returns the result.
 
+    Parameters:
+    query (str): The SQL query to execute.
+
+    Returns:
+    DataFrame: The result of the query as a pandas DataFrame.
+    """
     original_cwd = os.getcwd()
     os.chdir('data')
     
@@ -62,6 +61,16 @@ def execute_duckdb_query(query):
 
 
 def get_json_output(llm_response):
+    """
+    This function cleans the AI's response, extracts the JSON content, and checks if it contains a SQL query or an error message.
+
+    Parameters:
+    llm_response (str): The AI's response.
+
+    Returns:
+    tuple: A tuple where the first element is a boolean indicating if the response contains a SQL query (True) or an error message (False), 
+           and the second element is the SQL query or the error message.
+    """
 
     # remove bad characters and whitespace
     llm_response_no_escape = llm_response.replace('\\n', ' ').replace('\n', ' ').replace('\\', '').replace('\\', '').strip() 
@@ -81,6 +90,20 @@ def get_json_output(llm_response):
 
 
 def get_reflection(client,full_prompt,llm_response,model):
+    """
+    This function generates a reflection prompt when there is an error with the AI's response. 
+    It then sends this reflection prompt to the Groq API and retrieves the AI's response.
+
+    Parameters:
+    client (Groq): The Groq API client.
+    full_prompt (str): The original prompt that was sent to the AI.
+    llm_response (str): The AI's response to the original prompt.
+    model (str): The AI model to use for the response.
+
+    Returns:
+    str: The content of the AI's response to the reflection prompt.
+    """
+
     reflection_prompt = '''
     You were giving the following prompt:
 
@@ -108,6 +131,21 @@ def get_reflection(client,full_prompt,llm_response,model):
 
 
 def get_summarization(client,user_question,df,model,additional_context):
+    """
+    This function generates a summarization prompt based on the user's question and the resulting data. 
+    It then sends this summarization prompt to the Groq API and retrieves the AI's response.
+
+    Parameters:
+    client (Groqcloud): The Groq API client.
+    user_question (str): The user's question.
+    df (DataFrame): The DataFrame resulting from the SQL query.
+    model (str): The AI model to use for the response.
+    additional_context (str): Any additional context provided by the user.
+
+    Returns:
+    str: The content of the AI's response to the summarization prompt.
+    """
+
     prompt = '''
     A user asked the following question pertaining to local database tables:
     
@@ -131,74 +169,84 @@ def get_summarization(client,user_question,df,model,additional_context):
 
 
 def main():
+    """
+    The main function of the application. It handles user input, controls the flow of the application, 
+    and displays the results on the Streamlit interface.
+    """
     
-    groq_api_key = os.environ['GROQ_API_KEY']
-    client = Groqcloud(
-        # This is the default and can be omitted
+    # Get the Groq API key and create a Groq client
+    groq_api_key = st.secrets["GROQ_API_KEY"]
+    client = Groq(
         api_key=groq_api_key,
-        base_url="https://api.groqcloud.com"
+        base_url=st.secrets["GROQ_BASE_URL"]
     )
 
+    # Set up the Streamlit interface
     spacer, col = st.columns([5, 1])  
     with col:  
         st.image('groqcloud_darkmode.png')
 
     st.title("DuckDB Query Generator")
-    st.write("Ask questions about your DuckDB data, powered by Groq")
+    st.write('Welcome! Feel free to ask questions about the data contained in the `employees.csv` and `purchases.csv` files. You might ask about specific employee details or inquire about purchase records. For example, you could ask "Who are the employees?" or "What are the most recent purchases?". The application matches your question to SQL queries to provide accurate and relevant results. Enjoy exploring the data!')
 
+    # Set up the customization options
     st.sidebar.title('Customization')
     additional_context = st.sidebar.text_input('Enter additional summarization context for the LLM here (i.e. write it in spanish):')
     model = st.sidebar.selectbox(
         'Choose a model',
         ['mixtral-8x7b-32768', 'llama2-70b-4096']
     )
-    max_num_reflections = st.sidebar.slider('Max reflections:', 0, 20, value=10)
-    conversational_memory = st.sidebar.slider('Conversational memory length:', 1, 10)
+    max_num_reflections = st.sidebar.slider('Max reflections:', 0, 10, value=5)
 
-
+    # Load the base prompt
     with open('prompts/base_prompt.txt', 'r') as file:
         base_prompt = file.read()
+
+    # Get the user's question
     user_question = st.text_input("Ask a question:")
 
-    if 'user_question_history' not in st.session_state:
-        st.session_state['user_question_history'] = []
-
-    if 'chatbot_answer_history' not in st.session_state:
-        st.session_state['chatbot_answer_history'] = []
-
-    
+    # If the user has asked a question, process it
     if user_question:
-        st.session_state['user_question_history'].append(user_question)
-        full_prompt = get_conversational_history(base_prompt,st.session_state['user_question_history'],st.session_state['chatbot_answer_history'],conversational_memory)
+        # Generate the full prompt for the AI
+        full_prompt = base_prompt.format(user_question=user_question)
         
+        # Get the AI's response
         llm_response = chat_with_groq(client,full_prompt,model)
-        st.session_state['chatbot_answer_history'].append(llm_response)
 
+        # Try to process the AI's response
         valid_response = False
         i=0
         while valid_response is False and i < max_num_reflections:
             try:
+                # Check if the AI's response contains a SQL query or an error message
                 is_sql,result = get_json_output(llm_response)
                 if is_sql:
+                    # If the response contains a SQL query, execute it
                     results_df = execute_duckdb_query(result)
                     valid_response = True
                 else:
+                    # If the response contains an error message, it's considered valid
                     valid_response = True
             except:
+                # If there was an error processing the AI's response, get a reflection
                 llm_response = get_reflection(client,full_prompt,llm_response,model)
                 i+=1
-                #st.write('BAD:',llm_response)
 
+        # Display the result
         try:
             if is_sql:
+                # If the result is a SQL query, display the query and the resulting data
                 st.markdown("```sql\n" + result + "\n```")
                 st.markdown(results_df.to_html(index=False), unsafe_allow_html=True)
 
+                # Get a summarization of the data and display it
                 summarization = get_summarization(client,user_question,results_df,model,additional_context)
                 st.write(summarization.replace('$','\\$'))
             else:
+                # If the result is an error message, display it
                 st.write(result)
         except:
+            # If there was an error displaying the result, display an error message
             st.write("ERROR:", 'Could not generate valid SQL for this question')
             st.write(llm_response)
             
